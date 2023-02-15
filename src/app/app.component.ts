a2b2a007b83503720880c9ecb80533da1d9ac6c3
Marion-tech <cainael@gmail.com>
2023-02-15 19:45:12 +0100

    Service-Provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AlimentService } from './aliment.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Services-providers-injector';
